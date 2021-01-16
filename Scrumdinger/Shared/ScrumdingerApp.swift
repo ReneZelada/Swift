0341eee6a37d9bc8a093791227e078e3d0c6e58b
René Zelada <razp92@gmail.com>
2021-01-15 18:44:51 -0600

    SwiftUI

---
+++ Scrumdinger/Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by René Zelada on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
