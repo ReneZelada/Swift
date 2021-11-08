a60e69b0a24495e1df6b0a41737f22f1cb2f744d
René Zelada <rzelada@digitalgeko.com>
2021-11-08 12:48:50 -0600

    carrusel

---
+++ carusell/carusell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  carusell
+//
+//  Created by René Zelada on 10/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
